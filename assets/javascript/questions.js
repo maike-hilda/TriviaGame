3db95bd499958419f508c990212b13308780d04c
maike-hilda <maikescherer@gmail.com>
2018-04-08 15:49:49 -0700

    made constructor for questions

--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,43 +1,43 @@
-const questions = [
-    {
-        question: "What is You know who's middle name?",
-        answers: ["Severus", "Horrace", "Tom", "Marvolo"],
-        correct: "Marvolo",
-        funFact: "Lord Voldemort's full name is Tom Marvolo Riddle, " +
-            "Tom after his muggle dad and Marvolo after his grandfather.",
-        gifKeyword: "Tom Riddle Voldemort"
-     }, {
-        question: "What does the K in J.K. Rowling stand for?", 
-        answers: ["Kathrine", "Kathleen", "Katrina", "Kaley"],
-        correct: "Kathleen", 
-        funFact: "Joanne Kathleen's last name is pronounced Row-ling, like rowing a boat.",
-        gifKeyword: "J.K. Rowling"
-     }, {
-        question: "What is the name of Hermione and Ron's son?",
-        answers: ["Fred", "Albus", "Hugo", "Sirius"], 
-        correct: "Hugo", 
-        funFact: "Ron and Hermione have two children named Hugo and Rose.",
-        gifKeyword: "Ron Hermione Hugo"
-     }, {
-        question: "Who is Neville Longbottom married to?",
-        answers: ["Hannah Abbott", "Luna Lovegood", "Lavender Brown", "Ginny Weasley"],
-        correct: "Hannah Abbott",
-        funFact: "Neville Longbottom's  wife Hannah Abbott is landlady of the Leaky Cauldron, " +
-            "entrance to Diagon Alley in London.",
-        gifKeyword: "Neville Longbottom Hannah Abbott"
-     }, {
-        question: "Which of the following items was not a horcrux?",
-        answers: ["Rowena Ravenclaw's Lost Diadem", "Godric Gryffindor's Sword", 
-            "Salazar Slytherin's Locket", "Helga Hufflepuff's Cup"],
-        correct: "Godric Gryffindor's Sword",
-        funFact: "Voldemort would have loved to collect all four founders objects, but he could never " +
-            "find the sword as it only presents itself to a worthy Gryffindor.",
-        gifKeyword: "Slytherin's Sword"
-    }, {
-        question: "What shape does Hermione's Patronus take?",
-        answers: ["Beaver", "Lynx", "Otter", "Doe"],
-        correct: "Otter",
-        funFact: "Ron's patronus is a Jack Russell Terrier which are known to chase otters.",
-        gifKeyword: "Hermione Otter Patronus"
-    }
-];
+// question constructor
+function Question(question, answers, correct, funFact, gifKeyword) {
+    this.question = question; // string
+    this.answers = answers; // array of strings
+    this.correct = correct; // string
+    this.funFact = funFact; // string
+    this.gifKeyword = gifKeyword // string
+}
+
+// question array
+let questions = [];
+
+questions.push(new Question("What is You know who's middle name?", 
+    ["Severus", "Horrace", "Tom", "Marvolo"], "Marvolo", 
+    "Lord Voldemort's full name is Tom Marvolo Riddle, Tom after his muggle dad and Marvolo after his grandfather.",
+    "Tom Riddle Voldemort"));
+
+questions.push(new Question("What does the K in J.K. Rowling stand for?", 
+    ["Kathrine", "Kathleen", "Katrina", "Kaley"], "Kathleen", 
+    "Joanne Kathleen's last name is pronounced Row-ling, like rowing a boat.",
+    "J.K. Rowling"));
+
+questions.push(new Question("What is the name of Hermione and Ron's son?",
+    ["Fred", "Albus", "Hugo", "Sirius"], "Hugo", 
+    "Ron and Hermione have two children named Hugo and Rose.",
+    "Ron Hermione Hugo"));
+
+questions.push(new Question("Who is Neville Longbottom married to?",
+    ["Hannah Abbott", "Luna Lovegood", "Lavender Brown", "Ginny Weasley"], "Hannah Abbott",
+    "Neville Longbottom's  wife Hannah Abbott is landlady of the Leaky Cauldron, entrance to Diagon Alley in London.",
+    "Neville Longbottom Hannah Abbott"));
+
+questions.push(new Question("Which of the following items was not a horcrux?",
+    ["Rowena Ravenclaw's Lost Diadem", "Godric Gryffindor's Sword", 
+        "Salazar Slytherin's Locket", "Helga Hufflepuff's Cup"], "Godric Gryffindor's Sword",
+    "Voldemort would have loved to collect all four founders objects, but he could never " +
+        "find the sword as it only presents itself to a worthy Gryffindor.",
+    "Slytherin's Sword"));
+
+questions.push(new Question("What shape does Hermione's Patronus take?",
+    ["Beaver", "Lynx", "Otter", "Doe"], "Otter",
+    "Ron's patronus is a Jack Russell Terrier which are known to chase otters.",
+    "Hermione Otter Patronus"));
